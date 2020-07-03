44ee1539ef0775e60e59e8caf2ff033c47a30b20
sudhanshu-cedcoss <sudhanshukumarrai@cedcoss.com>
2020-07-03 12:14:07 +0530

    relative url

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,3 @@
 document.getElementById('select').onchange = function() {
-  window.location = "/test/developer-test/index.php?search=" + this.value;
+  window.location = "index.php?search=" + this.value;
 };
